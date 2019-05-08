ed122a3892d9324f68105b013acdd50046a247b4
Gustavo Leme <gleme@nevada.unr.edu>
2019-05-08 01:05:36 -0300

    add firstName and lastName required constrain

--- src/domain/model/user.js
+++ src/domain/model/user.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  firstName: { type: String },
-  lastName: { type: String },
   email: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   birthDate: { type: Date }
 });
 

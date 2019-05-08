254af2de84288be5f93ee8a28e55b2f80211e103
Gustavo Leme <gleme@nevada.unr.edu>
2019-05-08 01:03:42 -0300

    add test cases require minimum functionality

--- tests/unit/domain/model/user.spec.js
+++ tests/unit/domain/model/user.spec.js
@@ -13,10 +13,36 @@ describe('User (unit)', () => {
     });
   });
 
-  context('Valid data', () => {
+  context('valid data', () => {
     describe('constructor', () => {
       it('should create a user', () => {
         expect(new User(givenUser())).to.be.instanceOf(User);
+      });
+    });
+  });
+
+  context('invalid data', () => {
+    let email, firstName, lastName;
+    beforeEach(() => {
+      email = givenUser().email;
+      firstName = givenUser().firstName;
+      lastName = givenUser().lastName;
+    });
+
+    describe('constructor', () => {
+      it('should not create an user without email', () => {
+        const user = new User({ firstName, lastName });
+        expect(user.validateSync()).to.be.instanceOf(Error);
+      });
+
+      it('should not create an user without first name', () => {
+        const user = new User({ email, lastName });
+        expect(user.validateSync()).to.be.instanceOf(Error);
+      });
+
+      it('should not create an user without last name', () => {
+        const user = new User({ email, firstName });
+        expect(user.validateSync()).to.be.instanceOf(Error);
       });
     });
   });

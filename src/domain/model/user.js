fc4b8ab3f29419c0bd3419cc9fd68d8ccfcfee47
Gustavo Leme <gleme@nevada.unr.edu>
2019-05-09 16:48:19 -0300

    GREEN - implement to recognize emails with @

--- src/domain/model/user.js
+++ src/domain/model/user.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  email: { type: String, required: true, unique: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: value => {
+        return value.indexOf('@') !== -1;
+      }
+    }
+  },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   birthDate: { type: Date }
